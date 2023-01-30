45ab51f34491cd803db6e4c48d3d5a0ad94f35e0
Matej Leskovšek <matej.leskovsek@vizija.si>
2023-01-30 10:27:21 +0100

    Added project files for Geolocator-PWA PoC

---
+++ geolocator-pwa/src/app/app.component.ts
@@ -0,0 +1,41 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'Geolocator PWA';
+  geolocation = 'pritisni iskanje geolokacije';
+  fullResponse = '';
+
+  getGeolocation(): void {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position: GeolocationPosition) => {
+        if (position) {
+          this.geolocation = '(' + position.coords.latitude + ', ' + position.coords.longitude + ')';
+        }
+        this.fullResponse = JSON.stringify(this.cloneAsObject(position));
+      },
+        (error: GeolocationPositionError) => {
+          console.log(error);
+          this.fullResponse = JSON.stringify(this.cloneAsObject(error));
+          this.geolocation = 'dostop zavrnjen'
+        });
+    } else {
+      alert("Geolocation is not supported by this browser.");
+    }
+  }
+
+  cloneAsObject(obj: any): any {
+    if (obj === null || !(obj instanceof Object)) {
+      return obj;
+    }
+    var temp: any = (obj instanceof Array) ? [] : {};
+    for (var key in obj) {
+        temp[key] = this.cloneAsObject(obj[key]);
+    }
+    return temp;
+  }
+}
---
+++ geolocator-pwa/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { DxButtonModule } from 'devextreme-angular/ui/button';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    DxButtonModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
